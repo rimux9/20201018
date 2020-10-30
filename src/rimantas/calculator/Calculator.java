e237b162e0bc8d8b756f3bd5149c5562033a4cf3
rimux9 <rmatuliokas@gmail.com>
2020-10-30 11:53:54 +0200

    Calculator

---
+++ src/rimantas/calculator/Calculator.java
@@ -0,0 +1,40 @@
+package rimantas.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        int result;
+        char smb;
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter first num  ");
+        num1 = reader.nextInt();
+
+        System.out.print("Enter smb ");
+        smb = reader.next().charAt(0);
+
+        System.out.print("Enter second num  ");
+        num2 = reader.nextInt();
+
+        switch(smb) {
+            case '+'   : result = num1 + num2;
+                break;
+            case '-'   : result = num1 - num2;
+             break;
+            case '*'   : result = num1 * num2;
+                break;
+            case '/'   : result = num1 / num2;
+                break;
+            default:
+                return;
+
+        }
+        System.out.println("\nResult is :\n" + num1 + " " + smb + " " + num2 + " = " + result);
+
+    }
+
+}
